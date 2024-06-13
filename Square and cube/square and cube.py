6396c0b03921af656e619e318d25cf79737991d9
Sheru <03.mashraf@gmail.com>
2024-06-13 09:33:53 +0530

    sq and cube

---
+++ Square and cube/square and cube.py
@@ -0,0 +1,5 @@
+num1 = int(input("Enter a number to find its square and cube: "))
+sq = num1 **2
+cube = num1 **3
+print("Square of given no is " + str(sq))
+print("Cube of given no is " + str(cube))
