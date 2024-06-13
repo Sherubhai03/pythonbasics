cd49f1ff0217d425aa3c542ede068126d62f6eb2
Sherubhai03 <03.mashraf@gmail.com>
2024-06-13 09:45:33 +0530

    Delete sq and cube directory

--- sq and cube/sq and cube.py
+++
@@ -1,5 +0,0 @@
-num1 = int(input("Enter a number to find its square and cube: "))
-sq = num1 **2
-cube = num1 **3
-print("Square of given no is " + str(sq))
-print("Cube of given no is " + str(cube))
