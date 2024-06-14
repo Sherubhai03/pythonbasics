cd62aa13b6acd55ee5e11c9c1f933ea4045d5a92
Sheru <03.mashraf@gmail.com>
2024-06-14 09:33:47 +0530

    sq and cube

---
+++ fibonachi series/Fibonacci sequence.py
@@ -0,0 +1,17 @@
+Num1 = int(input("Enter the number up to which you want fibonacci series: "))
+Num2 = 0
+Num3 = 1
+fib = 0
+Num4 = 2
+print(Num2)
+print(Num3)
+while Num4 < Num1:
+    fib = Num2 + Num3
+    print(fib)
+    Num2 = Num3
+    Num3 = fib
+    Num4 = Num4 + 1
+
+
+
+
